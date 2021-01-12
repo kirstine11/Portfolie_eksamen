8296e3b60d49531ca248d2a508d32331ff4afdbc
Kirstine Krogshede <kirstinekrogshede@MacBook-Pro-tilhrende-Kirstine.local>
2021-01-12 16:44:28 +0100

    commit 7

--- ux.js
+++ ux.js
@@ -9,7 +9,7 @@ function carousel() {
     }
     slideIndex++;
     if (slideIndex > x.length) {
-        slideIndex = 1
+        slideIndex = 1;
     }
     x[slideIndex - 1].style.display = "block";
     setTimeout(carousel, 2000);
